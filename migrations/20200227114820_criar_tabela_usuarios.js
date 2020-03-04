833318c9c8473636ddae2c4e7ec66d928c2a3e40
Tiago Cruz <tiago.unity.cruz@gmail.com>
2020-03-04 14:39:34 -0300

    conta default na migraçao

--- migrations/20200227114820_criar_tabela_usuarios.js
+++ migrations/20200227114820_criar_tabela_usuarios.js
@@ -6,7 +6,7 @@ exports.up = function(knex) {
     table.specificType("email", "varchar").notNullable().unique()
     table.specificType("senha", "varchar").notNullable()
     table.specificType("tipo", "varchar").notNullable().defaultTo("SUP")
-  }).raw(`alter table \"usuarios\" add constraint \"usuarios_tipo_check\" check (tipo='SUP' or tipo='LAR' or tipo='PROF')`)
+  }).raw(`alter table \"usuarios\" add constraint \"usuarios_tipo_check\" check (tipo='SUP' or tipo='LAR' or tipo='PROF'); insert into \"usuarios\" (nome, email, senha, tipo) values (\'lar\', \'lar\', \'$2b$12$Lsj0OZn28e5yz4zf87EiLuUIMO2KtJIJQR20/3U5h9l8sBSzWYVL2\', \'LAR\')`)
 };
 
 exports.down = function(knex) {
